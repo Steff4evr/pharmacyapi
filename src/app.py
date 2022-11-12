05a7f30b99476fa47f79898693e324b86e8868bc
Steffy Johnson <steffy.johnson@Steffys-MacBook-Pro.local>
2022-11-12 22:12:56 +1100

    updated

--- src/app.py
+++ src/app.py
@@ -4,13 +4,13 @@ from datetime import date, timedelta
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import update,desc
+from sqlalchemy import update, desc
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 
 app = Flask(__name__)
 
-#Setup configurations for the app
+# Setup configurations for the app
 app.config['JSON_SORT_KEYS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://steffy:password@127.0.0.1:5432/pharmacymanagement'
 app.config['JWT_SECRET_KEY'] = 'Steffys Pharmacy Management'
@@ -21,67 +21,83 @@ ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
-#Model for Pharmacist. 
-#It contains the Pharmacists that use the pharmacy management system
+# Model for Pharmacist.
+# It contains the Pharmacists that use the pharmacy management system
+
+################################################################
+#                      MODEL & SCHEMA DEF                      #
+################################################################
+
+
 class Pharmacist(db.Model):
-#Table name 
+    # Table name
     __tablename__ = 'pharmacist'
 
-#Field names for the pharmacist table
+# Field names for the pharmacist table
 
-#Primary key
+# Primary key
     pharmacist_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-#email which should be unique    
+# email which should be unique
     emailid = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-#Foreign key reference to the Pharmacist ID     
+# Foreign key reference to the Pharmacist ID
     pharm_rel = db.relationship(
         'PurchaseOrder', backref='purchaseorder', cascade='all, delete-orphan', lazy='dynamic')
 
 # Schema for Pharmacist model
+
+
 class PharmacistSchema(ma.Schema):
     class Meta:
         fields = ('pharmacist_id', 'name', 'emailid', 'password')
 
 # Model for Medincine list table.
 # This table holds the medicine master list and its details
+
+
 class MedicineList(db.Model):
-    #table name
+    # table name
     __tablename__ = 'medicinelist'
-#Columns in the model
-#primary key
+# Columns in the model
+# primary key
     med_id = db.Column(db.Integer, primary_key=True)
     med_name = db.Column(db.String)
     med_type = db.Column(db.String, nullable=False)
     med_dose = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
-    #Foreign key reference to the med_id
+    # Foreign key reference to the med_id
     med_rel = db.relationship(
         'MedicineStock', backref='medicinelist', cascade='all, delete-orphan', lazy='dynamic')
 
 # Schema for the MedicineList
+
+
 class MedicineListSchema(ma.Schema):
     class Meta:
         fields = ('med_id', 'med_name', 'med_type', 'med_dose', 'description')
 
 # Model for MedicineStock
 # This table holds the mdeicine stock information
+
+
 class MedicineStock(db.Model):
-    #table name
+    # table name
     __tablename__ = 'medicinestock'
-#columns for the tabble
-#primary_key
+# columns for the tabble
+# primary_key
     med_stockid = db.Column(db.Integer, primary_key=True)
-    #foreign key constraint reference to the med_id field in the medicine list table. This is to ensure that there are no stock other than the medicines from the master list
+    # foreign key constraint reference to the med_id field in the medicine list table. This is to ensure that there are no stock other than the medicines from the master list
     med_id = db.Column(db.Integer, db.ForeignKey(
         'medicinelist.med_id'), unique=True, nullable=False)
     quantity = db.Column(db.Numeric, nullable=False)
     price_per_unit = db.Column(db.Numeric, nullable=False)
     description = db.Column(db.String)
-    #foreign key reference to med_stockid
+    # foreign key reference to med_stockid
     medstock_rel = db.relationship(
         'PurchaseOrder', backref='purchaseorder1', cascade='all, delete-orphan', lazy='dynamic')
+
+# Schema for Medicinestock
 
 
 class MedicineStockSchema(ma.Schema):
@@ -89,17 +105,25 @@ class MedicineStockSchema(ma.Schema):
         fields = ('med_stockid', 'med_id', 'quantity',
                   'price_per_unit', 'description')
 
+# Model for purchase order
+# Handles the customer purchase orders.
+
 
 class PurchaseOrder(db.Model):
+    # table name
     __tablename__ = 'purchaseorder'
-
+# primary key
     purchaseorder_id = db.Column(db.Integer, primary_key=True)
+    # Foreign key constraint towards stockid in medicinestock
     med_stockid = db.Column(db.Integer, db.ForeignKey(
         'medicinestock.med_stockid'), nullable=False)
+# Foreign key constraint towards pharmacist id in pharmacist table
     pharmacist_id = db.Column(db.Integer, db.ForeignKey(
         'pharmacist.pharmacist_id'), nullable=False)
     price = db.Column(db.Numeric, nullable=False)
     quantity = db.Column(db.Numeric, nullable=False)
+
+# schema for purchase order
 
 
 class PurchaseOrderSchema(ma.Schema):
@@ -108,19 +132,25 @@ class PurchaseOrderSchema(ma.Schema):
                   'pharmacist_id', 'price', 'meddose', 'quantity')
 
 ################################################################
+#                           SEEDING                            #
+################################################################
+# Define a custom CLI (terminal) command for creating the tables defined in model.
 
 
-# Define a custom CLI (terminal) command
 @app.cli.command('createpharmtables')
 def create_db():
     db.create_all()
     print("Pharmacy Management Tables created")
+
+# Define a custom CLI (terminal) command for dropping the tables defined in model.
 
 
 @app.cli.command('droppharmtables')
 def drop_db():
     db.drop_all()
     print("Pharmacy Management Tables dropped")
+
+# Define the CLI for seeding all tables in pharmacy management app
 
 
 @app.cli.command('seedpharmtables')
@@ -225,17 +255,21 @@ def seed_db():
     db.session.commit()
     print('Tables seeded')
 
-    ################################################################
+################################################################
+#                           ENDPOINTS                          #
+################################################################
 
+#
 @app.route('/auth/register/', methods=['POST'])
-@jwt_required()
+#@jwt_required()
 def auth_register():
     try:
         # Register new pharmacist user
         user = Pharmacist(
-            emailid = request.json['emailid'],
-            password = bcrypt.generate_password_hash(request.json['password']).decode('utf8'),
-            name = request.json['name']
+            emailid=request.json['emailid'],
+            password=bcrypt.generate_password_hash(
+                request.json['password']).decode('utf8'),
+            name=request.json['name']
         )
         # Add and commit user to DB
         db.session.add(user)
@@ -243,22 +277,27 @@ def auth_register():
         # Respond to client
         return PharmacistSchema(exclude=['password']).dump(user), 201
     except IntegrityError:
-        return {'error': 'Email address already in use'}, 409    
+        return {'error': 'Email address already in use'}, 409
 
-#User login and authentication
+# User login and authentication
+
+
 @app.route('/auth/login/', methods=['POST'])
 def auth_login():
     # Find a user by email address
     stmt = db.select(Pharmacist).filter_by(emailid=request.json['emailid'])
     user = db.session.scalar(stmt)
     # If user exists and password is correct
-    if user and bcrypt.check_password_hash(user.password, request.json['password']):        
-        token = create_access_token(identity=str(user.pharmacist_id), expires_delta=timedelta(days=1))
+    if user and bcrypt.check_password_hash(user.password, request.json['password']):
+        token = create_access_token(identity=str(
+            user.pharmacist_id), expires_delta=timedelta(days=1))
         return {'email': user.emailid, 'token': token}
     else:
         return {'error': 'Invalid email or password'}, 401
-        
+
 # Display medicine stock
+
+
 @app.route('/medstock/')
 def display_stock():
     medicine_stock_schema = MedicineStockSchema(many=True)
@@ -270,6 +309,8 @@ def display_stock():
     return jsonify(result)
 
 # Display List of medicines and its details
+
+
 @app.route('/medlist/')
 def display_medlist():
     medicine_list_schema = MedicineListSchema(many=True)
@@ -281,11 +322,14 @@ def display_medlist():
     return jsonify(result)
 
 # Display purchase orders
-@app.route('/po/')
+
+
+@app.route('/purchaseorder/')
 def display_po():
     po_schema = PurchaseOrderSchema(many=True)
     # Fetch all the records of the medicines
-    po_list = PurchaseOrder.query.order_by(desc(PurchaseOrder.purchaseorder_id)).all()
+    po_list = PurchaseOrder.query.order_by(
+        desc(PurchaseOrder.purchaseorder_id)).all()
     # convert the medicine list data from db into JSON format and store them in result variable
     result = po_schema.dump(po_list)
     # Retun the data in JSON format
@@ -316,18 +360,20 @@ def update_pharm():
         return {'error': 'Invalid Input'}, 400
 
 # Insert medicine to stock
+
+
 @app.route('/addmedtostock/', methods=['POST'])
 @jwt_required()
 def add_med():
     try:
-        med = MedicineStock(            
+        med = MedicineStock(
             med_id=request.json['medid'],
-            price_per_unit=request.json['price'],            
+            price_per_unit=request.json['price'],
             quantity=request.json['quantity'],
             description=request.json['description']
         )
         if db.session.query(MedicineList).filter(MedicineList.med_id == med.med_id).count() == 0:
-            return {'Not_found': 'Medicine id is invalid, Please enter a valid medicine that is present in the medicine list'}, 400        
+            return {'Not_found': 'Medicine id is invalid, Please enter a valid medicine that is present in the medicine list'}, 400
         # Add and command the medicine to DB
         db.session.add(med)
         db.session.commit()
@@ -353,11 +399,11 @@ def update_medicine():
         if med.quantity < 0 or med.price_per_unit < 0:
             return {'Value error': 'The quantity or price has to be valid positve numbers'}, 400
         # Check if the stock record is already present.
-        elif db.session.query(MedicineStock).filter(MedicineStock.med_stockid == med.med_stockid).count() > 0 :
+        elif db.session.query(MedicineStock).filter(MedicineStock.med_stockid == med.med_stockid).count() > 0:
             db.session.query(MedicineStock).filter(MedicineStock.med_stockid == med.med_stockid).update(
                 {MedicineStock.price_per_unit: med.price_per_unit, MedicineStock.quantity: med.quantity}, synchronize_session=False)
         else:
-            return {'Not_found': 'There are no matching medicine to  update in stock'}, 400        
+            return {'Not_found': 'There are no matching medicine to  update in stock'}, 400
         db.session.commit()
         # Respond to client
         return {'Success': 'Successfully Updated the stock'}, 201
@@ -366,10 +412,12 @@ def update_medicine():
     except:
         return {'error': 'Invalid Input'}, 400
 
-# Add to PurchaseOrder - This endpoint is used to handle customer purchase orders. 
-@app.route('/purchaseorder/', methods=['POST'])
+# Add to PurchaseOrder - This endpoint is used to handle customer purchase orders.
+
+
+@app.route('/createpurchaseorder/', methods=['POST'])
 @jwt_required()
-def purchaseorder():
+def createpurchaseorder():
     try:
         med = PurchaseOrder(
             med_stockid=0,
@@ -411,6 +459,7 @@ def purchaseorder():
         return {'error': ' The Pharmacist ID is incorrect'}, 400
     except:
         return {'error': 'Invalid Input'}, 400
+
 
 @app.route('/')
 def index():
